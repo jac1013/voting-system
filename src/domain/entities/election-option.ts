cd3747f8a5e58b3d09514457534cbd5a3c7468fb
Joseph Arrieta <jac.wshmstr@gmail.com>
2021-05-20 20:17:44 -0500

    Add constructor for election option

--- src/domain/entities/election-option.ts
+++ src/domain/entities/election-option.ts
@@ -1,5 +1,16 @@
+import { Election } from './election';
+import { v4 as uuidv4 } from 'uuid';
+
 export class ElectionOption {
   id: string;
   choiceId: number;
   title: string;
+  election: Election;
+
+  constructor(choiceId: number, title: string, election: Election) {
+    this.id = uuidv4();
+    this.choiceId = choiceId;
+    this.title = title;
+    this.election = election;
+  }
 }
