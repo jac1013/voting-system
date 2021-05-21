fc4bd16b4d0d6607b039728b6af484e91aea16ab
Joseph Arrieta <jac.wshmstr@gmail.com>
2021-05-20 20:14:13 -0500

    Add Voter constructor

--- src/domain/entities/voter.ts
+++ src/domain/entities/voter.ts
@@ -2,4 +2,10 @@ export class Voter {
   nationalId: string;
   firstName: string;
   lastName: string;
+
+  constructor(nationalId: string, firstName: string, lastName: string) {
+    this.nationalId = nationalId;
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 }
